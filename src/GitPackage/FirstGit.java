08730a8d56dc57da40a084d4727ab7af95a447cb
Shahealam Kasar <kasarshahealam@gmail.com>
2022-12-27 18:42:27 +0530

    First Commit

---
+++ src/GitPackage/FirstGit.java
@@ -0,0 +1,9 @@
+package GitPackage;
+public class FirstGit {
+
+	public static void main(String[] args) {
+		System.out.println("First Git Program");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author SHAHEALAM
+ *
+ */
+module GitCheck {
+}
